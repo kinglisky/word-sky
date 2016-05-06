04dc2a5d285a18000d27997c39ba53103f9ef761
kinglisky <kinglisky@qq.com>
2016-05-06 14:36:34 +0800

    改coding端口

--- app.js
+++ app.js
@@ -72,7 +72,7 @@ io.on('connection', (socket) => {
 
 });
 
-
-server.listen(3000, () => {
+var port = process.env.VCAP_APP_PORT || 3000;
+server.listen(port, () => {
   console.log('the server run in the 3000');
 });
